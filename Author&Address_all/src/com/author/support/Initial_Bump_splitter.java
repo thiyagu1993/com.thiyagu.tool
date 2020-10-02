79baa92acce8fc519fdb61455c223f40cb20ecb1
Thiyagarajan_lenovo <rthiyagara@gmail.com>
2020-10-02 20:09:23 +0530

    Initial Update

--- Author&Address_all/src/com/author/support/Initial_Bump_splitter.java
+++ Author&Address_all/src/com/author/support/Initial_Bump_splitter.java
@@ -1,11 +1,61 @@
 package com.author.support;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Initial_Bump_splitter {
 
 	public static String bump_splitter(String s) {
 		
+		Pattern in1 = Pattern.compile("\\b[A-Z][A-Z][A-Z]. \\w+\\b|\\b[A-Z][A-Z][A-Z] \\w+\\b");
+		Pattern in = Pattern.compile("\\b[A-Z][A-Z]. \\w+\\b|\\b[A-Z][A-Z] \\w+\\b");
+		Pattern in2 = Pattern.compile("\\b[A-Z]\\w+[A-Z]\\w+\\b");
 		
+		Matcher m = in.matcher(s);
+		Matcher m1 = in1.matcher(s);
+		Matcher m2 = in2.matcher(s);
+		
+		if(m1.find()){
+			
+			int icount=1;
+			StringBuilder sb = new StringBuilder(s);
+			
+			sb.insert(icount, ' ');
+			sb.insert(icount+2, ' ');
+			
+			return sb.toString();
+				
+		}
+				
+		if(m.find()){
+			
+			int icount=1;
+			StringBuilder sb = new StringBuilder(s);
+			
+			sb.insert(icount, ' ');
+			System.out.println("entering 2"+sb.toString());
+			return sb.toString();
+				
+		}
+		
+		if(m2.find()){
+			int icount=0;
+			StringBuilder sb = new StringBuilder(s);
+			char[] jj = s.toCharArray();
+			
+			for(int h=0;h<s.indexOf(" ");h++){
+					
+				if(Character.isUpperCase(jj[h+1])){
+			
+					icount = h+1;
+								
+				}
+			}
+			
+			sb.insert(icount, ' ');
+			
+			return sb.toString();
+		}
 		
 		return s;
 	}
--- Author&Address_all/src/misc/Normalizer_author.java
+++ Author&Address_all/src/misc/Normalizer_author.java
@@ -3,17 +3,17 @@ package misc;
 public class Normalizer_author {
 
 	public static String normal_author(String s){
+	
+		String[] Symbols= {".-","‐","’","-","Jr","Sr","III","II","  ",".","–","‑"," ","Mª"};
 		
-		String[] Symbols= {"‐","’","-","Jr","Sr","III","II","  ",".","–","‑"};
-		
-		String[] rep_symbols={"-","'","-","","","","",""," ","-","-"};
+		String[] rep_symbols={"-","-","'","-","","","",""," "," ","-","-"," ","Ma"};
 		
 		for(int i=0;i<Symbols.length;i++){
 	
 		s=s.replace(Symbols[i], rep_symbols[i]);	
 		
 		}
-	
+		System.out.println("normal "+s.replace("  ", " "));
 		return s.replace("  ", " ");
 	}
 		
